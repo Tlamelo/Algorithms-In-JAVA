090dd76466710aa72d9762edfd186b5f78cf8ee3
Tlamelo Titus Diphale <tlamelo.diphale@gmail.com>
2021-03-03 08:10:04 +0200

    Palindrome Solution

---
+++ Integers/PalindromeNumber.java
@@ -0,0 +1,28 @@
+package Integers;
+
+public class PalindromeNumber {
+
+    /**
+     * Determine whether an integer is a palindrome.An integer is a palindrome
+     * when it reads the same backward as forward.
+     */
+
+     public static boolean isPalindrome(int x){
+         if(x < 0 || (x % 10 == 0 && x != 0)){
+             return false;
+         }
+         int revertedNumber = 0;
+         while(x > revertedNumber){
+            revertedNumber=revertedNumber * 10 + x%10;
+            x /=10;
+         }
+
+         return x == revertedNumber || x == revertedNumber/10;
+     }
+
+    public static void main(String[] args) {
+         
+        System.out.println(isPalindrome(123));
+     }
+    
+}
